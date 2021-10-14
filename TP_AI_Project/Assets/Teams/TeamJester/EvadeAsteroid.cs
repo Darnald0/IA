54d962a4793786ae5bbf0c7b6aeb3693b0f72924
Bailek <nimsgernleo@gmail.com>
2021-10-14 12:05:58 +0200

    wip asteroid

--- TP_AI_Project/Assets/Teams/TeamJester/EvadeAsteroid.cs
+++ TP_AI_Project/Assets/Teams/TeamJester/EvadeAsteroid.cs
@@ -29,26 +29,26 @@ namespace TeamJester
         }
         void Update()
         {
-            if (JesterController.instance == null || JesterController.instance._spaceShip == null) return;
+            if (controller == null || controller._spaceShip == null) return;
             for (int i = 0; i < nb_Raycast; i++)
             {
-                Vector2 dir = JesterController.instance._spaceShip.Velocity.normalized;
+                Vector2 dir = controller._spaceShip.Velocity.normalized;
                 Quaternion Rot = Quaternion.Euler(0, 0, max_angle * i / (nb_Raycast - 1));
                 dir = Rot * dir;
-                Debug.DrawRay(JesterController.instance._spaceShip.PositionWorld, dir*RaycastLenght , Color.white);
+                Debug.DrawRay(controller._spaceShip.PositionWorld, dir*RaycastLenght , Color.white);
 
-                raycastHit2DsRight[i] = Physics2D.Raycast(JesterController.instance._spaceShip.PositionWorld, dir, RaycastLenght, layerMask);
+                raycastHit2DsRight[i] = Physics2D.Raycast(controller._spaceShip.PositionWorld, dir, RaycastLenght, layerMask);
 
             }
 
             for (int i = 0; i < nb_Raycast; i++)
             {
-                Vector2 dir = JesterController.instance._spaceShip.Velocity.normalized;
+                Vector2 dir = controller._spaceShip.Velocity.normalized;
                 Quaternion Rot = Quaternion.Euler(0, 0, -(max_angle * i / (nb_Raycast - 1)));
                 dir = Rot * dir;
-                Debug.DrawRay(JesterController.instance._spaceShip.PositionWorld, dir * RaycastLenght, Color.white);
+                Debug.DrawRay(controller._spaceShip.PositionWorld, dir * RaycastLenght, Color.white);
 
-                raycastHit2DsLeft[i] = Physics2D.Raycast(JesterController.instance._spaceShip.PositionWorld, dir, RaycastLenght, layerMask);
+                raycastHit2DsLeft[i] = Physics2D.Raycast(controller._spaceShip.PositionWorld, dir, RaycastLenght, layerMask);
 
             }
 
@@ -56,35 +56,46 @@ namespace TeamJester
             {
                 if (raycastHit2DsRight[i].collider != null && i != 0)
                 {
-                    Vector2 dir = JesterController.instance._spaceShip.Velocity.normalized;
+                    Vector2 dir = controller._spaceShip.Velocity.normalized;
                     Quaternion Rot = Quaternion.Euler(0, 0, max_angle * i / (nb_Raycast - 1));
                     dir = Rot * dir;
-                    Debug.DrawRay(JesterController.instance._spaceShip.PositionWorld, dir, Color.yellow);
+                    Debug.DrawRay(controller._spaceShip.PositionWorld, dir, Color.yellow);
                     Debug.Log("detecte l'asteroid a droite");
                     // se deplace a gauche
                     if (raycastHit2DsRight[0].collider == null && raycastHit2DsLeft[0].collider == null)
                     {
-                        controller.tree
+                        controller.tree.SetVariable("Target", (SharedVector2)controller._spaceShip.Velocity.normalized);
+                    }
+                    else if(raycastHit2DsLeft[i].collider == null && i != 0)
+                    {
+                        Vector2 dirleft = controller._spaceShip.Velocity.normalized;
+                        Quaternion Rotleft = Quaternion.Euler(0, 0, -(max_angle * i / (nb_Raycast - 1)));
+                        dirleft = Rotleft * dirleft;
+                        controller.tree.SetVariable("Target", (SharedVector2)dirleft);
                     }
 
                 }
 
                 if (raycastHit2DsLeft[i].collider != null && i != 0)
                 {
-                    Vector2 dir = JesterController.instance._spaceShip.Velocity.normalized;
+                    Vector2 dir = controller._spaceShip.Velocity.normalized;
                     Quaternion Rot = Quaternion.Euler(0, 0, -(max_angle * i / (nb_Raycast - 1)));
                     dir = Rot * dir;
-                    Debug.DrawRay(JesterController.instance._spaceShip.PositionWorld, dir, Color.yellow);
+                    Debug.DrawRay(controller._spaceShip.PositionWorld, dir, Color.yellow);
                     Debug.Log("detecte l'asteroid a gauche");
                     // se deplace a droite 
+                    if (raycastHit2DsRight[0].collider == null && raycastHit2DsLeft[0].collider == null)
+                    {
+                        controller.tree.SetVariable("Target", (SharedVector2)dir);
+                    }
                 }
 
                 if (raycastHit2DsRight[0].collider != null && raycastHit2DsLeft[0].collider != null)
                 {
-                    Vector2 dir = JesterController.instance._spaceShip.Velocity.normalized;
+                    Vector2 dir = controller._spaceShip.Velocity.normalized;
                     Quaternion Rot = Quaternion.Euler(0, 0, (max_angle * 0 / (nb_Raycast - 1)));
                     dir = Rot * dir;
-                    Debug.DrawRay(JesterController.instance._spaceShip.PositionWorld, dir, Color.yellow);
+                    Debug.DrawRay(controller._spaceShip.PositionWorld, dir, Color.yellow);
                     Debug.Log("detecte l'asteroid au midle");
                     // 
                 }
--- TP_AI_Project/Assets/Teams/TeamJester/JesterShoot.cs
+++ TP_AI_Project/Assets/Teams/TeamJester/JesterShoot.cs
@@ -1,22 +1,22 @@
-using BehaviorDesigner.Runtime.Tasks;
+//using BehaviorDesigner.Runtime.Tasks;
 
-namespace TeamJester
-{
-    [TaskCategory("Jester")]
+//namespace TeamJester
+//{
+//    [TaskCategory("Jester")]
 
-    public class JesterShoot : Action
-    {
+//    public class JesterShoot : Action
+//    {
 
-        public override TaskStatus OnUpdate()
-        {
-            if(JesterController.instance._spaceShip.Energy >= JesterController.instance._spaceShip.ShootEnergyCost)
-            {
-                JesterController.instance.nextInputData.shoot = true;
-                return TaskStatus.Success;
-            }
-            return TaskStatus.Running;
-        }
+//        public override TaskStatus OnUpdate()
+//        {
+//            if(JesterController.instance._spaceShip.Energy >= JesterController.instance._spaceShip.ShootEnergyCost)
+//            {
+//                JesterController.instance.nextInputData.shoot = true;
+//                return TaskStatus.Success;
+//            }
+//            return TaskStatus.Running;
+//        }
 
-    }
+//    }
 
-}
+//}
