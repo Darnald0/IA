f581b6018ccf6ce8ee61f5cc8cfc5a005714a304
Nolan <nolan.jutard@gmail.com>
2021-10-15 09:47:43 +0200

    Fix shoot

--- TP_AI_Project/Assets/Teams/TeamJester/JesterShoot.cs
+++ TP_AI_Project/Assets/Teams/TeamJester/JesterShoot.cs
@@ -18,12 +18,13 @@ namespace TeamJester
         public override TaskStatus OnUpdate()
         {
             Vector2 dir = controller._spaceShip.Velocity.normalized;
-            Debug.DrawRay(controller._spaceShip.PositionWorld, dir * 5, Color.red);
+            Debug.DrawRay(new Vector3(controller._spaceShip.PositionWorld.x + dir.x*0.6f, controller._spaceShip.PositionWorld.y + dir.y*0.6f, 0), dir * 5, Color.red); ;
             mask = LayerMask.GetMask("Player");
-
-            RaycastHit2D hit = Physics2D.Raycast(controller._spaceShip.PositionWorld, dir, 5, mask);
-            if (hit.collider != null)
+            RaycastHit2D hit = Physics2D.Raycast(new Vector3(controller._spaceShip.PositionWorld.x + dir.x*0.6f, controller._spaceShip.PositionWorld.y + dir.y*0.6f, 0), dir, 5, mask);
+            if (hit.collider != null && new Vector2(hit.collider.transform.parent.position.x, hit.collider.transform.parent.position.y) != controller._spaceShip.Position)
             {
+                Debug.Log(hit.collider.gameObject.transform.parent.name);
+
                controller.nextInputData.shoot = true;
                return TaskStatus.Success;
             }
